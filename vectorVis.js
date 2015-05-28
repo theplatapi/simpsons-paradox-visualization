9efb1501d7d2778f76bc625be4129c03a042fc64
Allen Korenevsky <theplatapi@gmail.com>
2015-05-28 11:21:46 -0700

    Switched to a working scatterplot vis. Building off of this.

--- vectorVis.js
+++ vectorVis.js
@@ -13,30 +13,91 @@
 //F	272	6%	341	7%
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  var data = [
-    {
-      applicants: 0,
-      admitted: 0
-    },
-    {
-      applicants: 40,
-      admitted: 44
-    }
-  ];
+  var margin = {top: 20, right: 20, bottom: 30, left: 40},
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
 
-  var lineFunction = d3.svg.line()
-                            .x(function(data) { return data.applicants; })
-                            .y(function(data) { return data.admitted; })
-                            .interpolate("linear");
+  var x = d3.scale.linear()
+      .range([0, width]);
 
-  var svg = d3.select("#vectorVis")
-      .append("svg")
-        .attr('width', 400)
-        .attr('height', 400)
-      .append('path')
-        .attr('d', lineFunction(data))
-        .attr("stroke", "blue")
-        .attr("stroke-width", 2)
-        .attr("fill", "none");
+  var y = d3.scale.linear()
+      .range([height, 0]);
+
+  var color = d3.scale.category10();
+
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
+
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left");
+
+  var svg = d3.select("#vectorVis").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  d3.tsv("data.tsv", function(error, data) {
+    data.forEach(function(d) {
+      d.sepalLength = +d.sepalLength;
+      d.sepalWidth = +d.sepalWidth;
+    });
+
+    x.domain(d3.extent(data, function(d) { return d.sepalWidth; })).nice();
+    y.domain(d3.extent(data, function(d) { return d.sepalLength; })).nice();
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("x", width)
+        .attr("y", -6)
+        .style("text-anchor", "end")
+        .text("Sepal Width (cm)");
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Sepal Length (cm)");
+
+    svg.selectAll(".dot")
+        .data(data)
+        .enter().append("circle")
+        .attr("class", "dot")
+        .attr("r", 3.5)
+        .attr("cx", function(d) { return x(d.sepalWidth); })
+        .attr("cy", function(d) { return y(d.sepalLength); })
+        .style("fill", function(d) { return color(d.species); });
+
+    var legend = svg.selectAll(".legend")
+        .data(color.domain())
+        .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+    legend.append("rect")
+        .attr("x", width - 18)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", color);
+
+    legend.append("text")
+        .attr("x", width - 24)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text(function(d) { return d; });
+
+  });
 });
 
