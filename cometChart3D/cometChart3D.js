b3dc6b5017a5cfafb7630b9dec58d03d34bdabbe
Allen Korenevsky <theplatapi@gmail.com>
2015-06-03 23:01:05 -0700

    Axes working.

--- cometChart3D/cometChart3D.js
+++ cometChart3D/cometChart3D.js
@@ -8,15 +8,35 @@ $(function() {
   var gl = twgl.getWebGLContext($('#chart3D')[0]);
   var programInfo = twgl.createProgramInfo(gl, ["vs", "fs"]);
 
-  var bufferInfo = twgl.primitives.createCubeBufferInfo(gl, 2);
-  //console.log(arrays);
-  //var arrays = {
-  //  position: [1, 1, -1, 1, 1, 1, 1, -1, 1, 1, -1, -1, -1, 1, 1, -1, 1, -1, -1, -1, -1, -1, -1, 1, -1, 1, 1, 1, 1, 1, 1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1, 1, -1, 1, -1, -1, 1, 1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1, 1, -1, 1, -1, 1, 1, -1, 1, -1, -1, -1, -1, -1],
-  //  normal:   [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1],
-  //  texcoord: [1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1],
-  //  indices:  [0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12, 14, 15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23],
-  //};
-  //var bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
+  //var bufferInfo = twgl.primitives.createCubeBufferInfo(gl, 2);
+
+  var axes = {
+    position: [
+      0, 0, 0,
+      0, 1, 0,
+      1, 0, 0,
+      0, 0, 1
+    ],
+    indices: [
+      0, 1,
+      0, 2,
+      0, 3
+    ],
+    normal: [
+      0, 1, 0,
+      0, 1, 0,
+      0, 1, 0,
+      0, 1, 0
+    ],
+    texcoord: [
+      0, 0,
+      0, 1,
+      1, 0,
+      1, 1
+    ]
+  };
+
+  var bufferInfo = twgl.createBufferInfoFromArrays(gl, axes);
 
   var tex = twgl.createTexture(gl, {
     min: gl.NEAREST,
@@ -70,5 +90,6 @@ $(function() {
 
     requestAnimationFrame(render);
   }
+
   requestAnimationFrame(render);
 });
